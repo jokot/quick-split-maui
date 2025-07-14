9a0dec82ffa8b961bcf35f28cdc2bae5c2dc3202
jokot <jokotriyanto12345@gmail.com>
2025-07-14 16:03:10 +0700

    Initial commit of the quick_split project, including .NET MAUI setup with essential files such as App.xaml, AppShell, MainPage, and platform-specific configurations for Android, iOS, and Windows. Added resources for styles, images, and fonts, along with a .gitignore file to exclude user-specific and build files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using quick_split.Views;
+
+namespace quick_split;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(DetailsPage), typeof(DetailsPage));
+		Routing.RegisterRoute(nameof(AddBillPage), typeof(AddBillPage));
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace quick_split;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
